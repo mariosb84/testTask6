96dbda3a35527474ea4c50eeade47893a01eb33b
mariosb84 <mariosb84@mail.ru>
2024-06-07 18:00:41 +0300

    ItemController

---
+++ services/itemService/src/main/java/com/example/itemservice/domain/dto/ItemDto.java
@@ -0,0 +1,23 @@
+package com.example.itemservice.domain.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class ItemDto {
+
+    @Size(min = 5, max = 50, message = "Название заявки должно содержать от 5 до 50 символов")
+    @NotBlank(message = "Название заявки не может быть пустыми")
+    private String name;
+
+    @Size(min = 8, max = 255, message = "Длина текста должна быть от 8 до 255 символов")
+    private String itemText;
+
+}
