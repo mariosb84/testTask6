51b6a9343da0c1cf46e356ece916509d679f5653
mariosb84 <mariosb84@mail.ru>
2024-07-26 17:58:08 +0300

    ItemController

---
+++ services/itemService/src/main/resources/db/scripts/005_dml_insert_person_table.sql
@@ -0,0 +1,4 @@
+INSERT INTO person(person_login, person_password, person_email, person_phone) VALUES
+('User_Test', '12345678900', 'Us_Test@mail.ru', '+79211111111'),
+('Operator_Test', '12345678901', 'Op_Test@mail.ru', '+79211111112'),
+('Administrator_Test', '12345678902', 'Adm_Test@mail.ru', '+79211111113');
---
+++ services/itemService/src/main/resources/db/scripts/006_dml_insert_person_roles_table.sql
@@ -0,0 +1,4 @@
+INSERT INTO person_roles(person_id , role) VALUES
+(1, 'ROLE_USER'),
+(2, 'ROLE_OPERATOR'),
+(3, 'ROLE_ADMIN');
