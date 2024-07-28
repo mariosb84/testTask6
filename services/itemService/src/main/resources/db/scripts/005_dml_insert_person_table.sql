fa9321148dd1e04a307ab210af6744e48ab4dc9a
Sergei Baranov <mariosb84@mail.ru>
2024-07-28 20:38:57 +0300

    ItemController

--- services/itemService/src/main/resources/db/scripts/005_dml_insert_person_table.sql
+++ services/itemService/src/main/resources/db/scripts/005_dml_insert_person_table.sql
@@ -1,4 +1,4 @@
 INSERT INTO person(person_login, person_password, person_email, person_phone) VALUES
-('User_Test', '12345678900', 'Us_Test@mail.ru', '+79211111111'),
-('Operator_Test', '12345678901', 'Op_Test@mail.ru', '+79211111112'),
-('Administrator_Test', '12345678902', 'Adm_Test@mail.ru', '+79211111113');
+('User_Test', '$2a$10$zK8v4X5V2N0jlQhEImgweOG8YSvu/9cMHSjVisI7Y9UPo6cOCFjVG', 'Us_Test@mail.ru', '+79211111111'),
+('Operator_Test', '$2a$10$r0THmRmMxS3K39J2aQb1K.s5hWyGlEwjVPkNOd0stK65/iM8BJaqy', 'Op_Test@mail.ru', '+79211111112'),
+('Administrator_Test', '$2a$10$sp5LkLKgHcrgscYN20vVUe6/Uv2gg5pWPihXeqVi0F6X1QLUMvC.6', 'Adm_Test@mail.ru', '+79211111113');
